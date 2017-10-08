16094d45a050605530315a47c3e861b944e640fa
Dionisius Wisnu 2017103388 <diongakure@gmail.com>
2017-10-09 06:13:36 +0700

    HTML_Table

---
+++ proses.php
@@ -0,0 +1,12 @@
+<?php
+   echo $_POST['nama'];
+   echo "<br />";
+   echo $_POST['gender'];
+   echo "<br />";
+   echo $_POST['hobi'];
+   echo "<br />";
+   echo $_POST['select1'];
+   echo "<br />";
+   echo $_POST['pesan'];
+   echo "<br />";
+?>
